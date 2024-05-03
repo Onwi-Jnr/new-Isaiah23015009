7e0f4c2151c98b8d7f4d552a283387950f45a570
Onwi-Jnr <167882576+Onwi-Jnr@users.noreply.github.com>
2024-05-03 11:25:15 +0100

    Validation.py

---
+++ Control structure/Validation.py
@@ -0,0 +1,8 @@
+gender = input('Pls enter your gender ')
+age = int(input('PLs enter your age '))
+if gender == 'male' and age >= 18:
+    print(' you are a man ')
+elif gender== 'female' and age >= 18:
+    print('you are a female')
+else:
+    print('You are a minor')
